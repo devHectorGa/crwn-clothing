1524fe422798ced7685ddd33654b2837dad20e12
devHectorGa <contacto@devHector.Ga>
2019-11-27 20:36:27 -0500

    Header Component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import {
   Switch,
 } from 'react-router-dom'
 
-import './App.css';
+import './App.css'
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 
+import Header from './components/header/header.component'
+
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
@@ -20,4 +23,4 @@ function App() {
   );
 }
 
-export default App;
+export default App
