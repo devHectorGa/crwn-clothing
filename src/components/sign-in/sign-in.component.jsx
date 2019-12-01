ebedc9f12bc42f1bc894e905733894a307e060a8
devHectorGa <contacto@devHector.Ga>
2019-12-01 15:06:29 -0500

    Sign Up With Email and Password

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -41,7 +41,7 @@ class SignIn extends React.Component {
             name="password"
             type="password"
             label="password"
-            value={this.state.email}
+            value={this.state.password}
             handleChange={this.handleChange}
             required
           />
