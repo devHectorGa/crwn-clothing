a69aa1eca08a8e18fd33d65c7fa48bab3a282406
devHectorGa <contacto@devHector.Ga>
2019-12-01 11:15:28 -0500

    Sign In Component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import './App.css'
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import signInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 import Header from './components/header/header.component'
 
@@ -18,6 +19,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signIn' component={signInAndSignUp} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './sign-in-and-sign-up.styles.scss'
+
+import SignIn from '../../components/sign-in/sign-in.component'
+
+const signInAndSignUp = ()=>(
+  <SignIn />
+)
+
+export default signInAndSignUp
