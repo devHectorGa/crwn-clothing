e99ac68c593e8ded47a64d007659523d54b5bd0c
devHectorGa <contacto@devHector.Ga>
2019-12-12 10:07:21 -0500

    Improving Naming Of Component

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./collection.styles.scss";
+
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
+const CollectionPage = ({ match }) => (
+  <div className="collection-page">
+    <h2>collection PAGE</h2>
+  </div>
+);
+
+export default CollectionPage;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,13 +3,16 @@ import "./shop.styles.scss";
 import { Route } from "react-router-dom";
 
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
-import CategoryPage from "../category/category.component";
+import CollectionPage from "../collection/collection.component";
 
-const ShopPage = ({ match }) => (
-  <div className="shop-page">
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
-  </div>
-);
+const ShopPage = ({ match }) => {
+  console.log(match);
+  return (
+    <div className="shop-page">
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+  );
+};
 
 export default ShopPage;
