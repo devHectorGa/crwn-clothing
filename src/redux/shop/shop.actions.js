6d93f3379a59cbd395585b0f288a542efd025893
devHectorGa <Hetikor@outlook.com>
2019-12-26 17:18:11 -0500

    Redux Thunk

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,34 @@
 import ShopActionsTypes from "./shop.types";
+import {
+  firestore,
+  convertCollectionsSnapshotToMap
+} from "../../firebase/firebase.utils";
 
-export const updateCollections = collectionsMap => ({
-  type: ShopActionsTypes.UPDATE_COLLECTIONS,
+export const fetchCollecionsStart = collectionsMap => ({
+  type: ShopActionsTypes.FETCH_COLLECTIONS_START
+});
+
+export const fetchCollecionsSuccess = collectionsMap => ({
+  type: ShopActionsTypes.FETCH_COLLECTIONS_SUCCESS,
   payload: collectionsMap
 });
+
+export const fetchCollecionsFailure = errorMessage => ({
+  type: ShopActionsTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage
+});
+
+export const fetchCollectionsStartAsync = () => {
+  return dispatch => {
+    const collectionRef = firestore.collection("collections");
+    dispatch(fetchCollecionsStart());
+
+    collectionRef
+      .get()
+      .then(async snapshot => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        dispatch(fetchCollecionsSuccess(collectionsMap));
+      })
+      .catch(error => dispatch(fetchCollecionsFailure(error.message)));
+  };
+};
