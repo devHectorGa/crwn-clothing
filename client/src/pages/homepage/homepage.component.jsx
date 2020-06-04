5279cd196e4a75e2cee5be53b5faab82074ecce5
devHectorGa <contacto@devHector.Ga>
2020-06-04 13:31:47 -0500

    React Profiler

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { Profiler } from "react";
 import Directory from "../../components/directory/directory.component";
 
-const HomePage = () => <Directory />;
+const HomePage = () => (
+  <Profiler
+    id="Directory"
+    onRender={(id, phase, actualDuration) =>
+      console.log({ id, phase, actualDuration })
+    }
+  >
+    <Directory />
+  </Profiler>
+);
 
 export default HomePage;
